264e639e6fbc17dda8895df5fa6fb5a04b57b470
gatisstrauss97 <98421067+gatisstrauss97@users.noreply.github.com>
2023-05-10 01:42:14 +0300

    first half of the homework

--- Homework/src/main/java/io/codelex/flowcontrol/practice/LargestNumber.java
+++ Homework/src/main/java/io/codelex/flowcontrol/practice/LargestNumber.java
@@ -17,12 +17,22 @@ public class LargestNumber {
         System.out.print("Input the 3rd number: ");
         int num3 = in.nextInt();
 
+        if (num1 > num2 && num1 > num3) {
+            System.out.println(num1);
+        } else if (num2 > num3 && num2 > num1) {
+            System.out.println(num2);
+        } else if (num3 > num2 && num3 > num1) {
+            System.out.println(num3);
+        }
+
+
+    }
         /*
         todo - expected output:
         Input the 1st number: 25
         Input the 2nd number: 78
         Input the 3rd number: 87
          */
-    }
-
 }
+
+
---
+++ Homework/src/main/java/io/codelex/flowcontrol/practice/PhoneKeyPad.java
@@ -0,0 +1,47 @@
+package io.codelex.flowcontrol.practice;
+
+import java.util.Scanner;
+
+public class PhoneKeyPad {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String character;
+        System.out.println("Enter a series of characters:");
+        character = scanner.next().toLowerCase();
+
+        int digit = 0;
+        for (int i = 0; i < character.length(); i++) {
+            if (character.charAt(i) == 'a' || character.charAt(i) == 'b' || character.charAt(i) == 'c') {
+                System.out.print(digit = 2);
+            } else if (character.charAt(i) == 'd' || character.charAt(i) == 'e' || character.charAt(i) == 'f') {
+                System.out.print(digit = 3);
+            } else if (character.charAt(i) == 'g' || character.charAt(i) == 'h' || character.charAt(i) == 'i') {
+                System.out.print(digit = 4);
+            } else if (character.charAt(i) == 'j' || character.charAt(i) == 'k' || character.charAt(i) == 'l') {
+                System.out.print(digit = 5);
+            } else if (character.charAt(i) == 'm' || character.charAt(i) == 'n' || character.charAt(i) == 'o') {
+                System.out.print(digit = 6);
+            } else if (character.charAt(i) == 'p' || character.charAt(i) == 'q' || character.charAt(i) == 'r' || character.charAt(i) == 's') {
+                System.out.print(digit = 7);
+            } else if (character.charAt(i) == 't' || character.charAt(i) == 'u' || character.charAt(i) == 'v') {
+                System.out.print(digit = 8);
+            } else if (character.charAt(i) == 'w' || character.charAt(i) == 'x' || character.charAt(i) == 'y' || character.charAt(i) == 'z') {
+                System.out.print(digit = 9);
+            }
+        }
+//            switch (character.charAt(i)) {
+//                case 'a', 'b', 'c' -> digit = 2;
+//                case 'd', 'e', 'f' -> digit = 3;
+//                case 'g', 'h', 'i' -> digit = 4;
+//                case 'j', 'k', 'l' -> digit = 5;
+//                case 'm', 'n', 'o' -> digit = 6;
+//                case 'p', 'q', 'r', 's' -> digit = 7;
+//                case 't', 'u', 'v' -> digit = 8;
+//                case 'w', 'x', 'y', 'z' -> digit = 9;
+//
+//               Šis pieraksts ir daudz ērtāks :)
+//            }
+//            System.out.print(digit);
+    }
+}
